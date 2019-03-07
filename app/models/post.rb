0d22855517dc331949ee0dc00540952b197b8d5b
Rohan Nair <rrnair.98@gmail.com>
2019-03-07 13:29:46 +0530

    value for likes is always 0 although the query is being executed. Will fix it next commit.
    Signed-off-by: Rohan Nair <rrnair.98@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,12 @@ class Post < ApplicationRecord
   attr_accessor :likes
   def as_json(option = {})
     options ||= { except: %i[created_at updated_at user_id], include: [images: { only: %i[id image_url] }] }
-    self.likes = Like.where(post_id: post_id).count
+    self.likes = Like.where(post_id: self[:post_id]).count
+
     super(options)
   end
 
   def attributes
-    super.merge('likes' => likes)
+    super.merge('likes' => self.likes)
   end
 end
